fb83af167ec4d16ea6a1d96393d02a6215440421
DacD <dacdao01@gmail.com>
2019-04-05 16:29:52 -0400

    working

---
+++ precache-manifest.4966dbc242f9750cbab62a748fb807dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0cbd48b7603adf6d279",
+    "url": "/react-to-dos/static/js/runtime~main.e391af02.js"
+  },
+  {
+    "revision": "6e6d6759521b3eceddd0",
+    "url": "/react-to-dos/static/js/main.f0777c18.chunk.js"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/react-to-dos/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "6e6d6759521b3eceddd0",
+    "url": "/react-to-dos/static/css/main.540dffa5.chunk.css"
+  },
+  {
+    "revision": "6d13156d9f59bded556ea4140cfd3b95",
+    "url": "/react-to-dos/index.html"
+  }
+];
